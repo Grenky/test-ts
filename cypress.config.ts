fb35dff9a37581a2466d2579a1ba73ac857bd8d9
Grenky <demosoft62@gmail.com>
2023-07-17 18:27:09 +0300

    second

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  
   e2e: {
+      "reporter": "mochawesome",
+      "reporterOptions": {
+        "charts": true,
+        "overwrite": false,
+        "html": false,
+        "json": true,
+        "reportDir": "cypress/reports/mochawesome-report",
+    "watchForFileChanges": true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+}});
